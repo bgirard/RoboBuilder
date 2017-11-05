73e17ce625779b253448f1b0e665b7e7e39d14cc
Benoit Girard <b56girard@gmail.com>
2017-11-05 14:16:02 -0800

    Stub in game board

---
+++ scripts/robobuilder/GameBoard.js
@@ -0,0 +1,45 @@
+const GroundType = {
+  SPACE: {
+    id: 0,
+    name: 'space',
+  },
+  DIRT: {
+    id: 1,
+    name: 'dirt',
+  },
+};
+
+const Item = {
+  IRON_ORE: {
+  },
+  COPPER_ORE: {
+  },
+};
+
+class GameBoard {
+  constructor() {
+
+  }
+
+  getGroundType(x, y) {
+    if (x * x + y * y < 100) {
+      return GroundType.DIRT;
+    }
+    return GroundType.SPACE;
+  }
+
+  getOre(x, y) {
+    if (x == 0 && y == 0) {
+      return Item.IRON_ORE; 
+    }
+    return null;
+  }
+
+  getBuilding(x, y) {
+    return null;
+  }
+
+  getRobots() {
+    return [];
+  }
+}
