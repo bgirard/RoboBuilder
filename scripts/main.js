a9e14894de7a5fe90529edf48dde2f2b9b0c7a81
Benoit Girard <b56girard@gmail.com>
2017-11-05 15:35:30 -0800

    Basic keyboard movement

--- scripts/main.js
+++ scripts/main.js
@@ -46,4 +46,43 @@ function startGame() {
       // creates frame-independent tranformation
       //bunny.rotation += 0.1 * delta;
   });
+
+
+  const LEFT_ARROW = 37;
+  const UP_ARROW = 38;
+  const RIGHT_ARROW = 39;
+  const DOWN_ARROW = 40;
+  const KEY_W = 87;
+  const KEY_A = 65;
+  const KEY_S = 83;
+  const KEY_D = 68;
+
+  function moveFunc(x, y) {
+    boardSprite.x -= x * TILE_SIZE.x;
+    boardSprite.y -= y * TILE_SIZE.y;
+  }
+  keyboard(LEFT_ARROW).press = function() {
+    moveFunc(-1, 0);
+  };
+  keyboard(KEY_A).press = function() {
+    moveFunc(-1, 0);
+  };
+  keyboard(UP_ARROW).press = function() {
+    moveFunc(0, -1);
+  };
+  keyboard(KEY_W).press = function() {
+    moveFunc(0, -1);
+  };
+  keyboard(RIGHT_ARROW).press = function() {
+    moveFunc(1, 0);
+  };
+  keyboard(KEY_D).press = function() {
+    moveFunc(1, 0);
+  };
+  keyboard(DOWN_ARROW).press = function() {
+    moveFunc(0, 1);
+  };
+  keyboard(KEY_S).press = function() {
+    moveFunc(0, 1);
+  };
 }
