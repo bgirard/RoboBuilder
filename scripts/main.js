870b4d29471d855402893cc42e60edfecdb9413c
Benoit Girard <b56girard@gmail.com>
2017-11-05 17:19:01 -0800

    Fix factory order position

--- scripts/main.js
+++ scripts/main.js
@@ -13,7 +13,7 @@ function startGame() {
   
   let robot = gameBoard.createRobot(5 * TILE_SIZE.x, 5 * TILE_SIZE.y);
   let factory = gameBoard.createBuilding(14, 7);
-  robot.dropoffItem(factory.x, factory.y);
+  robot.dropoffItem(factory.getPixelX(), factory.getPixelY());
   
   let app = new PIXI.Application(GAME_SIZE.x, GAME_SIZE.y, {backgroundColor : 0x1099bb});
   document.getElementById('canvasContainer').appendChild(app.view);
