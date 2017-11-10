624d369c0bc88eb696ea5b94585d3b730699d1aa
Benoit Girard <b56girard@gmail.com>
2017-11-10 10:19:38 -0800

    Add factory crafting recipe

--- scripts/robobuilder/Item.js
+++ scripts/robobuilder/Item.js
@@ -27,6 +27,38 @@ const Item = {
       input: 'COPPER_ORE',
     }
   },
+  GEAR: {
+    name: 'Gear',
+    asset: 'ore_copper',
+    recipe: {
+      energy: 1,
+      input: 'IRON',
+    }
+  },
+  ELECTRONICS: {
+    name: 'Electronics',
+    asset: 'ore_copper',
+    recipe: {
+      energy: 1,
+      input: 'COPPER',
+    }
+  },
+  CIRCUIT: {
+    name: 'Circuit',
+    asset: 'ore_copper',
+    recipe: {
+      energy: 1,
+      input: 'ELECTRONICS',
+    }
+  },
+  FACTORY: {
+    name: 'Factory',
+    asset: 'ore_copper',
+    recipe: {
+      energy: 5,
+      input: 'CIRCUIT',
+    }
+  },
 };
 
 function getCraftable() {
